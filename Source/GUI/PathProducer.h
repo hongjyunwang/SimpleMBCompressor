45476e5e2103a9151d2bcebec70a69aad540c204
Hong-Jyun Wang <hongjyunwa@gmail.com>
2024-08-15 15:48:46 -0700

    Separate Files Refactor

---
+++ Source/GUI/PathProducer.h
@@ -0,0 +1,38 @@
+/*
+  ==============================================================================
+
+    PathProducer.h
+    Created: 15 Aug 2024 3:24:53pm
+    Author:  Hong Jyun Wang
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "FFTDataGenerator.h"
+#include "AnalyzerPathGenerator.h"
+#include "../PluginProcessor.h"
+
+struct PathProducer
+{
+    PathProducer(SingleChannelSampleFifo<SimpleMBCompAudioProcessor::BlockType>& scsf) :
+    leftChannelFifo(&scsf)
+    {
+        leftChannelFFTDataGenerator.changeOrder(FFTOrder::order2048);
+        monoBuffer.setSize(1, leftChannelFFTDataGenerator.getFFTSize());
+    }
+    void process(juce::Rectangle<float> fftBounds, double sampleRate);
+    juce::Path getPath() { return leftChannelFFTPath; }
+private:
+    SingleChannelSampleFifo<SimpleMBCompAudioProcessor::BlockType>* leftChannelFifo;
+    
+    juce::AudioBuffer<float> monoBuffer;
+    
+    FFTDataGenerator<std::vector<float>> leftChannelFFTDataGenerator;
+    
+    AnalyzerPathGenerator<juce::Path> pathProducer;
+    
+    juce::Path leftChannelFFTPath;
+};
